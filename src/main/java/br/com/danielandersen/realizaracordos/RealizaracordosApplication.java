92a8972e8517872286e61193b325fc8452965fb0
Daniel Andersen <dan.musik@gmail.com>
2020-07-21 21:22:06 -0300

    inicial commit

---
+++ src/main/java/br/com/danielandersen/realizaracordos/RealizaracordosApplication.java
@@ -0,0 +1,13 @@
+package br.com.danielandersen.realizaracordos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealizaracordosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealizaracordosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/danielandersen/realizaracordos/RealizaracordosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danielandersen.realizaracordos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealizaracordosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
